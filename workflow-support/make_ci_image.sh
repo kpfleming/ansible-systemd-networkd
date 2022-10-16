1ce513f312a3b5f61215cbc0c72d163348d04a34
Kevin P. Fleming <kevin@km6g.us>
2022-10-16 18:35:50 -0400

    Finish CI setup

--- workflow-support/make_ci_image.sh
+++ workflow-support/make_ci_image.sh
@@ -3,17 +3,19 @@
 set -ex
 
 scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+base_image=${1}; shift
+image_name=${1}; shift
 
-lintdeps=(shellcheck)
+lint_deps=(shellcheck)
 
-c=$(buildah from quay.io/km6g-ci-images/python:bullseye-main)
+c=$(buildah from "${base_image}":bullseye-main)
 
 buildcmd() {
     buildah run --network host "${c}" -- "$@"
 }
 
 buildcmd apt-get update --quiet=2
-buildcmd apt-get install --yes --quiet=2 "${lintdeps[@]}"
+buildcmd apt-get install --yes --quiet=2 "${lint_deps[@]}"
 
 buildcmd apt-get autoremove --yes --purge
 buildcmd apt-get clean autoclean
@@ -24,7 +26,6 @@ buildcmd tox -eALL --notest --workdir /root/tox
 
 buildcmd rm -rf /root/.cache
 
-# shellcheck disable=SC2154 # image_name set in external environment
 if buildah images --quiet "${image_name}"; then
     buildah rmi "${image_name}"
 fi
