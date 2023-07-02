c1055d95f12465023374203e38620c1d5c49186d
Kevin P. Fleming <kevin@km6g.us>
2023-07-02 10:45:43 -0400

    Prepare for automated docs publishing.

---
+++ workflow-support/make_docs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+basedir="$(realpath "${scriptdir}/..")"
+
+rm -rf "${basedir}/docs"
+
+apt-get update --quiet=2
+apt-get install --yes --quiet=2 rsync
+
+pip3 install ansible
+
+ansible-galaxy collection install kpfleming.systemd_networkd
+
+pip3 install -r "${basedir}/docs-build/requirements.txt"
+
+"${basedir}/docs-build/build.sh"
+
+mv "${basedir}/docs-build/build/html" "${basedir}/docs"
+
+touch "${basedir}/docs/.nojekyll"
